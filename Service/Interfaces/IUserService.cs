19a646baea8ff1b7c92ab8635d0d15e6fa36d842
ThanhIsHereee <nquangthanh88@gmail.com>
2024-09-29 14:11:07 +0700

    commit n + 1

--- Service/Interfaces/IUserService.cs
+++ Service/Interfaces/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessObj.Models;
+using DataAccessObj.DTO.UserDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Service.Interfaces
 {
-    internal interface IUserService
+    public interface IUserService
     {
+        public Task<List<User>> GetAll();
+        public Task<User> GetUserInformation(string user);
+        public Task<List<User>> SearchByName(string FullName);
+        public Task<User> Update(User user);
+        public Task<bool> Remove(string userID);
+        public Task<User> Registration(RegisterDTO request);
+        public Task<string> Login(LoginDTO request);
+        public Task<User> CreateUser(CreateUser user);
+        public Task<Int32> countCustomers();
     }
 }
--- Service/Services/UserService.cs
+++ Service/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using Repositories;
+﻿using BusinessObj.Models;
+using DataAccessObj.DTO.UserDTO;
+using Repositories;
 using Service.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -16,5 +18,32 @@ namespace Service.Services
         {
             _repo = repo;
         }
+
+        public async Task<int> countCustomers()
+        => await _repo.countCustomers();
+
+        public async Task<User> CreateUser(CreateUser user)
+        => await _repo.CreateUser(user);
+
+        public async Task<List<User>> GetAll()
+        => await _repo.GetAll();
+
+        public async Task<User> GetUserInformation(string user)
+        => await _repo.GetUserInformation(user);
+
+        public async Task<string> Login(LoginDTO request)
+        => await _repo.Login(request);
+
+        public async Task<User> Registration(RegisterDTO request)
+        => await _repo.Registration(request);
+
+        public async Task<bool> Remove(string userID)
+        => await _repo.Remove(userID);
+
+        public async Task<List<User>> SearchByName(string FullName)
+        => await _repo.SearchByName(FullName);
+
+        public async Task<User> Update(User user)
+        => await _repo.Update(user);
     }
 }
